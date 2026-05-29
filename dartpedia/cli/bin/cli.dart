085f616b82cddfecb16b93015894d997ad2f5b84
Nikita Levin <94102667+kpyacahb4uk@users.noreply.github.com>
2026-05-29 23:20:40 +0300

    добавил импорт http и функцию getWikipediaArticle для запроса к апи википедии

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 const version = '0.0.1';
 
 void main(List<String> arguments) {
@@ -33,4 +34,18 @@ void searchWikipedia(List<String>? arguments) {
   print('Looking up articles about "$articleTitle". Please wait.');
   print('Here ya go!');
   print('(Pretend this is an article about "$articleTitle")');
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
 }
